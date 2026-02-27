28e5098046fb41425a0897587849ac43c5255100
Gabriel Resendiz <gabriel.resendizmx@gmail.com>
2026-02-27 15:46:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9018645918-97f8c",
+  "appId": "1:679427826417:web:b62df09839248009c17f50",
+  "apiKey": "AIzaSyAQBiEoVV87T8zDziqBqOB9OMnu24J6rrA",
+  "authDomain": "studio-9018645918-97f8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "679427826417"
+};
